f277a3078fb4d76e73a06ee69b3b245ef00afd04
Freeman-YFH <firegeo@gmail.com>
2023-04-08 12:25:57 +0100

    delete console.log

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -103,7 +103,7 @@ describe('GET - /api/reviews', () => {
     });
 });
 
-describe.only('GET - /api/reviews/:review_id/comments', () => {
+describe('GET - /api/reviews/:review_id/comments', () => {
     it('200: GET response with an array of comments for the given review_id', () => {
         return request(app)
             .get("/api/reviews/2/comments")
